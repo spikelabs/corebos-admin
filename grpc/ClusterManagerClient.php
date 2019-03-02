062d73e301932ad61f8f53beb73f24bed794c64b
geri <zeqirigeri@gmail.com>
2019-03-02 21:30:03 +0100

    Grpc client setup

---
+++ grpc/ClusterManagerClient.php
@@ -0,0 +1,32 @@
+<?php
+// GENERATED CODE -- DO NOT EDIT!
+
+namespace ;
+
+/**
+ */
+class ClusterManagerClient extends \Grpc\BaseStub {
+
+    /**
+     * @param string $hostname hostname
+     * @param array $opts channel options
+     * @param \Grpc\Channel $channel (optional) re-use channel object
+     */
+    public function __construct($hostname, $opts, $channel = null) {
+        parent::__construct($hostname, $opts, $channel);
+    }
+
+    /**
+     * @param \CreateClientRequest $argument input argument
+     * @param array $metadata metadata
+     * @param array $options call options
+     */
+    public function CreateClient(\CreateClientRequest $argument,
+      $metadata = [], $options = []) {
+        return $this->_simpleRequest('/ClusterManager/CreateClient',
+        $argument,
+        ['\CreateClientResponse', 'decode'],
+        $metadata, $options);
+    }
+
+}
---
+++ grpc/GrpcClient.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: geri
+ * Date: 19-03-02
+ * Time: 8.45.MD
+ */
+
+require_once base_path("grpc/GPBMetadata/Kubernetes.php");
+require_once base_path("grpc/ClusterManagerClient.php");
+require_once base_path("grpc/CreateClientRequest.php");
+require_once base_path("grpc/CreateClientResponse.php");
+require_once base_path("grpc/Database.php");
+require_once base_path("grpc/DatabasePvc.php");
+require_once base_path("grpc/Deployment.php");
+require_once base_path("grpc/DeploymentPvc.php");
+require_once base_path("grpc/Ingress.php");
+require_once base_path("grpc/Service.php");
+
+
+class GrpcClient
+{
+
+    private $client;
+
+    public function __construct()
+    {
+        $this->client = $client = new ClusterManagerClient(env("grpc_server"), [
+            'credentials' => Grpc\ChannelCredentials::createInsecure(),
+        ]);
+    }
+
+    public function createClient(CreateClientRequest $request){
+        list($reply, $status) = $this->client->CreateClient($request);
+
+        return ['reply' => $reply, 'status' => $status];
+    }
+
+}
