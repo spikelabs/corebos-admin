77f48c9b5c9ca0e1588bc3b1340566e9916784d7
geri <zeqirigeri@gmail.com>
2019-03-16 15:02:42 +0100

    testing environment variables

---
+++ deployment/run.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+echo "DB_HOST=$DB_HOST" > .env
+echo "DB_PORT=$DB_PORT" > .env
+echo "DB_DATABASE=$DB_DATABASE" > .env
+echo "DB_USERNAME=$DB_USERNAME" > .env
+echo "DB_PASSWORD=$DB_PASSWORD" > .env
+
+php artisan cache:clear
+php artisan config:cache
+
+/usr/bin/supervisord
