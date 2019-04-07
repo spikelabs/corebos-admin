26cfa60d087df9f02b35dda2ef3fbdd5c0bd76b7
geri <zeqirigeri@gmail.com>
2019-04-07 14:31:19 +0200

    protoc generated file fixes

--- grpc/ClusterManagerClient.php
+++ grpc/ClusterManagerClient.php
@@ -1,45 +1,46 @@
 <?php
 // GENERATED CODE -- DO NOT EDIT!
 
-namespace ;
-
-/**
- */
-class ClusterManagerClient extends \Grpc\BaseStub {
+namespace {
 
     /**
-     * @param string $hostname hostname
-     * @param array $opts channel options
-     * @param \Grpc\Channel $channel (optional) re-use channel object
      */
-    public function __construct($hostname, $opts, $channel = null) {
-        parent::__construct($hostname, $opts, $channel);
-    }
+    class ClusterManagerClient extends \Grpc\BaseStub {
 
-    /**
-     * @param \CreateClientDatabaseRequest $argument input argument
-     * @param array $metadata metadata
-     * @param array $options call options
-     */
-    public function CreateClientDatabase(\CreateClientDatabaseRequest $argument,
-      $metadata = [], $options = []) {
-        return $this->_simpleRequest('/ClusterManager/CreateClientDatabase',
-        $argument,
-        ['\ClientResponse', 'decode'],
-        $metadata, $options);
-    }
+        /**
+         * @param string $hostname hostname
+         * @param array $opts channel options
+         * @param \Grpc\Channel $channel (optional) re-use channel object
+         */
+        public function __construct($hostname, $opts, $channel = null) {
+            parent::__construct($hostname, $opts, $channel);
+        }
 
-    /**
-     * @param \CreateClientDeploymentRequest $argument input argument
-     * @param array $metadata metadata
-     * @param array $options call options
-     */
-    public function CreateClientDeployment(\CreateClientDeploymentRequest $argument,
-      $metadata = [], $options = []) {
-        return $this->_simpleRequest('/ClusterManager/CreateClientDeployment',
-        $argument,
-        ['\ClientResponse', 'decode'],
-        $metadata, $options);
-    }
+        /**
+         * @param \CreateClientDatabaseRequest $argument input argument
+         * @param array $metadata metadata
+         * @param array $options call options
+         */
+        public function CreateClientDatabase(\CreateClientDatabaseRequest $argument,
+                                             $metadata = [], $options = []) {
+            return $this->_simpleRequest('/ClusterManager/CreateClientDatabase',
+                $argument,
+                ['\ClientResponse', 'decode'],
+                $metadata, $options);
+        }
 
+        /**
+         * @param \CreateClientDeploymentRequest $argument input argument
+         * @param array $metadata metadata
+         * @param array $options call options
+         */
+        public function CreateClientDeployment(\CreateClientDeploymentRequest $argument,
+                                               $metadata = [], $options = []) {
+            return $this->_simpleRequest('/ClusterManager/CreateClientDeployment',
+                $argument,
+                ['\ClientResponse', 'decode'],
+                $metadata, $options);
+        }
+
+    }
 }
