627d32744cd234e7a1264c5934f85ac973cebee5
geri <zeqirigeri@gmail.com>
2019-04-20 16:25:12 +0200

    Add environment variables for app env and force https for production urls

--- deployment/run.sh
+++ deployment/run.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
+echo "APP_ENV=$APP_ENV" >> .env
 echo "DB_HOST=$DB_HOST" >> .env
 echo "DB_PORT=$DB_PORT" >> .env
 echo "DB_DATABASE=$DB_DATABASE" >> .env
 echo "DB_USERNAME=$DB_USERNAME" >> .env
 echo "DB_PASSWORD=$DB_PASSWORD" >> .env
 echo "GRPC_CLIENT=$GRPC_CLIENT" >> .env
-echo "APP_ENV=$APP_ENV" >> .env
 
 php artisan cache:clear
 php artisan config:cache
